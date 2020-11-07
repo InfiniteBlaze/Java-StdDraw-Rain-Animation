e930fde93425f1ff549189698a99c45d97bdec86
InfiniteBlaze <44420420+InfiniteBlaze@users.noreply.github.com>
2020-11-06 19:12:46 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,134 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        StdDraw.setScale(-2, +2);
+        StdDraw.enableDoubleBuffering();
+        double rx = 0, ry = 1.5;
+        double vx = 0.005;
+        double radius = 0.8;
+        int minX = -1;
+        int maxX = 2;
+        int minY = 1;
+        int maxY = 2;
+
+            double random_double1 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double2 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double3 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double4 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double5 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double6 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double7 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double8 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double9 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double10 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double11 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double12 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double13 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double14 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double15 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double16 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double17 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double18 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double19 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double20 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double21 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double22 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double23 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double24 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double25 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double26 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double27 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double28 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double29 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double30 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double31 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double32 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double33 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double34 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double35 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double36 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double37 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double38 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double39 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double40 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double41 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double42 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double43 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double44 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double45 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double46 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double47 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double48 = Math.random() * (maxY - minY + 1) + minY;
+            double random_double49 = Math.random() * (maxX - minX + 1) + minX;
+            double random_double50 = Math.random() * (maxY - minY + 1) + minY;
+
+            for (double t = 0.0; true; t += 0.005) {
+                StdDraw.clear();
+
+                StdDraw.setPenColor(StdDraw.DARK_GRAY);
+                StdDraw.filledSquare(0, 0, 2);
+                StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+                if (t <= 2) {
+                    StdDraw.line(random_double1 - t * 1.05, random_double2 - t * 2, (random_double1 - 0.06) - t * 1.05, (random_double2 - 0.1) - t * 2);
+                    StdDraw.line(random_double3 - t * 1.05, random_double4 - t * 2, (random_double3 - 0.06) - t * 1.05, (random_double4 - 0.1) - t * 2);
+                    StdDraw.line(random_double5 - t * 1.05, random_double6 - t * 2, (random_double5 - 0.06) - t * 1.05, (random_double6 - 0.1) - t * 2);
+                    StdDraw.line(random_double7 - t * 1.05, random_double8 - t * 2, (random_double7 - 0.06) - t * 1.05, (random_double8 - 0.1) - t * 2);
+                    StdDraw.line(random_double9 - t * 1.05, random_double10 - t * 2, (random_double9 - 0.06) - t * 1.05, (random_double10 - 0.1) - t * 2);
+                    StdDraw.line(random_double11 - t * 1.05, random_double12 - t * 2, (random_double11- 0.06) - t * 1.05, (random_double12 - 0.1) - t * 2);
+                    StdDraw.line(random_double13 - t * 1.05, random_double14 - t * 2, (random_double13 - 0.06) - t * 1.05, (random_double14 - 0.1) - t * 2);
+                    StdDraw.line(random_double15 - t * 1.05, random_double16 - t * 2, (random_double15 - 0.06) - t * 1.05, (random_double16 - 0.1) - t * 2);
+                    StdDraw.line(random_double17 - t * 1.05, random_double18 - t * 2, (random_double17 - 0.06) - t * 1.05, (random_double18 - 0.1) - t * 2);
+                    StdDraw.line(random_double19 - t * 1.05, random_double20 - t * 2, (random_double19 - 0.06) - t * 1.05, (random_double20 - 0.1) - t * 2);
+                    StdDraw.line(random_double21 - t * 1.05, random_double22 - t * 2, (random_double21 - 0.06) - t * 1.05, (random_double22 - 0.1) - t * 2);
+                    StdDraw.line(random_double23 - t * 1.05, random_double24 - t * 2, (random_double23- 0.06) - t * 1.05, (random_double24 - 0.1) - t * 2);
+                    StdDraw.line(random_double25 - t * 1.05, random_double26 - t * 2, (random_double25 - 0.06) - t * 1.05, (random_double26 - 0.1) - t * 2);
+                    StdDraw.line(random_double27 - t * 1.05, random_double28 - t * 2, (random_double27 - 0.06) - t * 1.05, (random_double28 - 0.1) - t * 2);
+                    StdDraw.line(random_double29 - t * 1.05, random_double30 - t * 2, (random_double29 - 0.06) - t * 1.05, (random_double30 - 0.1) - t * 2);
+                    StdDraw.line(random_double31 - t * 1.05, random_double32 - t * 2, (random_double31 - 0.06) - t * 1.05, (random_double32 - 0.1) - t * 2);
+                    StdDraw.line(random_double33 - t * 1.05, random_double34 - t * 2, (random_double33 - 0.06) - t * 1.05, (random_double34 - 0.1) - t * 2);
+                    StdDraw.line(random_double35 - t * 1.05, random_double36 - t * 2, (random_double35- 0.06) - t * 1.05, (random_double36 - 0.1) - t * 2);
+                    StdDraw.line(random_double37 - t * 1.05, random_double38 - t * 2, (random_double37 - 0.06) - t * 1.05, (random_double38 - 0.1) - t * 2);
+                    StdDraw.line(random_double39 - t * 1.05, random_double40 - t * 2, (random_double39 - 0.06) - t * 1.05, (random_double40 - 0.1) - t * 2);
+                    StdDraw.line(random_double41 - t * 1.05, random_double42 - t * 2, (random_double41 - 0.06) - t * 1.05, (random_double42 - 0.1) - t * 2);
+                    StdDraw.line(random_double43 - t * 1.05, random_double44 - t * 2, (random_double43 - 0.06) - t * 1.05, (random_double44 - 0.1) - t * 2);
+                    StdDraw.line(random_double45 - t * 1.05, random_double46 - t * 2, (random_double45 - 0.06) - t * 1.05, (random_double46 - 0.1) - t * 2);
+                    StdDraw.line(random_double47 - t * 1.05, random_double48 - t * 2, (random_double47- 0.06) - t * 1.05, (random_double48 - 0.1) - t * 2);
+                    StdDraw.line(random_double49 - t * 1.05, random_double50 - t * 2, (random_double49- 0.06) - t * 1.05, (random_double50 - 0.1) - t * 2);
+
+                    StdDraw.setPenColor(StdDraw.GRAY);
+
+                    StdDraw.setPenColor(0, 40, 5);
+                    StdDraw.filledRectangle(0, -1.5, 2, 0.5);
+
+                    if (Math.abs(rx + vx) > 2 - radius) vx = -vx;
+
+                    rx = rx + vx;
+
+                    StdDraw.setPenColor(StdDraw.GRAY);
+                    StdDraw.filledCircle(rx, ry, radius);
+                    StdDraw.filledCircle(rx + 1, ry, radius);
+                    StdDraw.filledCircle(rx - 1, ry, radius);
+                    StdDraw.filledCircle(rx + 2, ry, radius);
+                    StdDraw.filledCircle(rx - 2, ry, radius);
+                    StdDraw.filledCircle(rx + 3, ry, radius);
+                    StdDraw.filledCircle(rx - 3, ry, radius);
+                    StdDraw.setPenColor(StdDraw.BLACK);
+                    StdDraw.filledRectangle(-1,-1,0.25,0.2);
+                    StdDraw.filledRectangle(-0.85,-0.6,0.05,0.1);
+                    double[] x = {-1.4,-1,-0.6};
+                    double[] y = {-0.8, -0.55, -0.8};
+                    StdDraw.filledPolygon(x,y);
+
+                } else {
+                    if (t >= 2) {
+                        t = 0;
+                    }
+                }
+                StdDraw.show();
+                StdDraw.pause(1);
+            }
+        }
+    }
+
+
